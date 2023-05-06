45cfae79837e966a35868f8feabd5e6d561d2f3d
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2023-05-06 08:38:08 +0530

    fix: Optimized Import

--- src/test/java/com/example/bookstore/service/BookServiceTest.java
+++ src/test/java/com/example/bookstore/service/BookServiceTest.java
@@ -9,19 +9,15 @@ import com.example.bookstore.service.impl.BookServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
